3f47e7eb08c7a58221970586dafcee4582dbd0a0
Pushti <pushtikadia@gmail.com>
2025-12-01 14:01:51 +0530

    changed 09_solution.py

--- 02_conditionals/09_solution.py
+++ 02_conditionals/09_solution.py
@@ -3,4 +3,4 @@ year = 2024
 if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
     print(f"{year} is a leap year.")
 else:
-    print(f"{year} is not a leap year.")
+    print(f"{year} is not a leap year. ")
